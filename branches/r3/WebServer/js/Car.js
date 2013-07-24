ffb05f10ad17d545933e8073e456afa1857cf62b
leventkaragol@gmail.com <leventkaragol@gmail.com@c91ced5c-9e9e-11e6-9eac-1c7dd7574ca4>
2013-07-24 22:01:51 +0000

    Araç fiziği ile ilgili çalışmalar yapıldı

--- branches/r3/WebServer/js/Car.js
+++ branches/r3/WebServer/js/Car.js
@@ -1,5 +1,48 @@
 ﻿can.Construct("CarGame.Car", {}, {
     Element: null,
+    Shift: 1,
+    Speed: 0,
+    ShiftSpecs: [{ Shift: "R", Min: -50, Max: 1 },
+                 { Shift: "1", Min: 0, Max: 49 },
+                 { Shift: "2", Min: 30, Max: 109 },
+                 { Shift: "3", Min: 90, Max: 159 },
+                 { Shift: "4", Min: 140, Max: 209 },
+                 { Shift: "5", Min: 190, Max: 259 },
+                 { Shift: "6", Min: 240, Max: 340 }],
+    AccelerationSpecs: [{ Min: 0, Max: 10, Acceleration: 50 },
+                        { Min: 10, Max: 20, Acceleration: 25 },
+                        { Min: 20, Max: 30, Acceleration: 14.29 },
+                        { Min: 30, Max: 40, Acceleration: 10 },
+                        { Min: 40, Max: 50, Acceleration: 7.69 },
+                        { Min: 50, Max: 60, Acceleration: 6.25 },
+                        { Min: 60, Max: 70, Acceleration: 5 },
+                        { Min: 70, Max: 80, Acceleration: 4.17 },
+                        { Min: 80, Max: 90, Acceleration: 3.57 },
+                        { Min: 90, Max: 100, Acceleration: 3.03 },
+                        { Min: 100, Max: 110, Acceleration: 2.63 },
+                        { Min: 110, Max: 120, Acceleration: 2.27 },
+                        { Min: 120, Max: 130, Acceleration: 2 },
+                        { Min: 130, Max: 140, Acceleration: 1.75 },
+                        { Min: 140, Max: 150, Acceleration: 1.56 },
+                        { Min: 150, Max: 160, Acceleration: 1.37 },
+                        { Min: 160, Max: 170, Acceleration: 1.2 },
+                        { Min: 170, Max: 180, Acceleration: 1.06 },
+                        { Min: 180, Max: 190, Acceleration: 0.94 },
+                        { Min: 190, Max: 200, Acceleration: 0.84 },
+                        { Min: 200, Max: 210, Acceleration: 0.75 },
+                        { Min: 210, Max: 220, Acceleration: 0.66 },
+                        { Min: 220, Max: 230, Acceleration: 0.58 },
+                        { Min: 230, Max: 240, Acceleration: 0.52 },
+                        { Min: 240, Max: 250, Acceleration: 0.45 },
+                        { Min: 250, Max: 260, Acceleration: 0.4 },
+                        { Min: 260, Max: 270, Acceleration: 0.35 },
+                        { Min: 270, Max: 280, Acceleration: 0.31 },
+                        { Min: 280, Max: 290, Acceleration: 0.27 },
+                        { Min: 290, Max: 300, Acceleration: 0.24 },
+                        { Min: 300, Max: 310, Acceleration: 0.21 },
+                        { Min: 310, Max: 320, Acceleration: 0.19 },
+                        { Min: 320, Max: 330, Acceleration: 0.16 },
+                        { Min: 330, Max: 340, Acceleration: 0.14 }],
     init: function ()
     {
         this.Element = new THREE.Object3D();
@@ -63,68 +106,152 @@
 
         this.car.Element.add(backRightWheel);
     },
-    HandleKeyboard: function (keyboardState)
+    Animate: function (keyboardState, deltaTime)
     {
-        var directionForward = false;
-        var directionBack = false;
+        this.HandleKeyboard(keyboardState, deltaTime);
 
+        this.CheckShiftChange();
+
+
+        if (this.Speed > 0)
+        {
+            this.Forward(deltaTime);
+        }
+        else if (this.Speed < 0)
+        {
+            this.Backward(deltaTime);
+        }
+
+        $("#SpeedText").text(Math.round(this.Speed, 2));
+        $("#ShiftText").text(this.Shift);
+    },
+    HandleKeyboard: function (keyboardState, deltaTime)
+    {
         if (keyboardState.pressed("up"))
         {
-            this.Forward();
-
-            directionForward = true;
+            if (this.Speed > 0)
+            {
+                this.Speed += this.CalculateAcceleration(this.Shift, this.Speed, false) * deltaTime * 7;
+            }
+            else
+            {
+                this.Speed += this.CalculateAcceleration(this.Shift, this.Speed, true) * deltaTime * 7;
+            }
         }
 
         if (keyboardState.pressed("down"))
         {
-            this.Backward();
-
-            directionBack = true;
+            if (this.Speed < 0)
+            {
+                this.Speed -= this.CalculateAcceleration(this.Shift, this.Speed, false) * deltaTime * 7;
+            }
+            else
+            {
+                this.Speed -= this.CalculateAcceleration(this.Shift, this.Speed, true) * deltaTime * 7;
+            }
         }
 
-        if (directionForward && !directionBack || !directionForward && directionBack)
+        if (this.Speed != 0)
         {
+            if (!keyboardState.pressed("up") && !keyboardState.pressed("down"))
+            {
+                // idle
+
+                if (this.Speed > 0)
+                {
+                    this.Speed -= 0.25;
+                }
+                else
+                {
+                    this.Speed += 0.25;
+                }
+            }
+
             if (keyboardState.pressed("left"))
             {
-                this.TurnLeft(directionBack);
+                this.TurnLeft();
             }
 
             if (keyboardState.pressed("right"))
             {
-                this.TurnRight(directionBack);
+                this.TurnRight();
             }
         }
     },
-    Forward: function ()
+    Forward: function (deltaTime)
     {
-        this.Element.translateZ(-0.25);
+        this.Element.translateZ(-1 * this.Speed / (3.6 / deltaTime));
     },
-    Backward: function ()
+    Backward: function (deltaTime)
     {
-        this.Element.translateZ(0.25);
+        this.Element.translateZ(-1 * this.Speed / (3.6 / deltaTime));
     },
-    TurnLeft: function (directionBack)
+    TurnLeft: function ()
     {
         var angle = Math.PI / 180;
 
         // Geri giderken arabanın burnu ters tarafa dönmeli
-        if (directionBack)
+        if (this.Speed < 0)
         {
             angle *= -1;
         }
 
         this.Element.rotateOnAxis(new THREE.Vector3(0, 1, 0), angle);
     },
-    TurnRight: function (directionBack)
+    TurnRight: function ()
     {
         var angle = -1 * Math.PI / 180;
 
         // Geri giderken arabanın burnu ters tarafa dönmeli
-        if (directionBack)
+        if (this.Speed < 0)
         {
             angle *= -1;
         }
 
         this.Element.rotateOnAxis(new THREE.Vector3(0, 1, 0), angle);
+    },
+    CheckShiftChange: function ()
+    {
+        if (this.Speed > this.ShiftSpecs[this.Shift].Max && this.Shift != 6)
+        {
+            // Güncel hız, güncel vitesin maksimum hızından fazlaysa ve vites zaten 6. viteste değilse, vites arttırımı gerekiyor
+            this.Shift++;
+        }
+        else if (this.Speed < this.ShiftSpecs[this.Shift].Min && this.Shift != 0)
+        {
+            // Güncel hız, güncel vitesin minimum hızından azsa ve vites zaten geri viteste değilse, vites azaltımı gerekiyor
+            this.Shift--;
+        }
+    },
+    CalculateAcceleration: function (shift, speed, forBreak)
+    {
+        if (forBreak)
+        {
+            return 10;
+        }
+        else
+        {
+            if (shift == 0)
+            {
+                if (this.Speed > -50)
+                {
+                    return 5;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+
+            for (var i = 0; i < this.AccelerationSpecs.length; i++)
+            {
+                if (speed >= this.AccelerationSpecs[i].Min && speed <= this.AccelerationSpecs[i].Max)
+                {
+                    return this.AccelerationSpecs[i].Acceleration;
+                }
+            }
+
+            return 0;
+        }
     }
 });
--- branches/r3/WebServer/js/Scene.js
+++ branches/r3/WebServer/js/Scene.js
@@ -1,7 +1,7 @@
 ﻿can.Construct("CarGame.Scene", {}, {
     Renderer: null,
     Element: null,
-    init: function ()
+    init: function (stats)
     {
         // HTML'e, üzerine Render edilmiş görüntüleri yansıtacağımız ve tüm sayfayı kaplayan bir Div ekliyoruz
         var body = $("body");
@@ -16,7 +16,37 @@
         container.css("width", "100%");
         container.css("height", "100%");
         container.css("overflow", "hidden");
+        container.css("zindex", "2");
+
+        body.append("<span id='SpeedText' />");
         
+        var speedText = $("#SpeedText");
+
+        speedText.css("position", "absolute");
+        speedText.css("left", "100px");
+        speedText.css("top", "100px");
+        speedText.css("zindex", "1");
+
+        body.append("<span id='ShiftText' />");
+
+        var shiftText = $("#ShiftText");
+
+        shiftText.css("position", "absolute");
+        shiftText.css("left", "100px");
+        shiftText.css("top", "150px");
+        shiftText.css("zindex", "1");
+
+        body.append("<div id='Stats' />");
+
+        var statsDiv = $("#Stats");
+
+        statsDiv.css("position", "absolute");
+        statsDiv.css("left", "0px");
+        statsDiv.css("top", "0px");
+        statsDiv.css("zindex", "1");
+
+        statsDiv.append(stats.domElement);
+
         this.Renderer = new THREE.WebGLRenderer({ antialias: true });
 
         this.Renderer.setSize(container.width(), container.height());
