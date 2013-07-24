91a8856719a68654c804a8029f5f9fd03915106d
leventkaragol@gmail.com <leventkaragol@gmail.com@c91ced5c-9e9e-11e6-9eac-1c7dd7574ca4>
2013-07-24 18:34:45 +0000

    * canJS ile Object Oriented bir yapıya geçildi * Zemin rengi açıldı

---
+++ branches/r2/WebServer/js/PerspectiveCamera.js
@@ -0,0 +1,13 @@
+﻿can.Construct("CarGame.PerspectiveCamera", {}, {
+    Element: null,
+    init: function ()
+    {
+        var aspectRatio = $("#Container").width() / $("#Container").height();
+
+        this.Element = new THREE.PerspectiveCamera(45, aspectRatio, 0.01, 2000);
+
+        this.Element.position.set(0, 10, 20);
+
+        this.Element.lookAt(new THREE.Vector3(0, 5, 0));
+    }
+});
