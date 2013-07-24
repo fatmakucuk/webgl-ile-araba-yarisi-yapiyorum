b87b4e169a5d046b5f7d8572b5712ff4284ad309
leventkaragol@gmail.com <leventkaragol@gmail.com@c91ced5c-9e9e-11e6-9eac-1c7dd7574ca4>
2013-07-24 18:38:25 +0000

    r2 için demo dosyaları eklendi

---
+++ wiki/demo/r2/js/Ground.js
@@ -0,0 +1,14 @@
+﻿can.Construct("CarGame.Ground", {}, {
+    Element: null,
+    init: function ()
+    {
+        this.Element = new THREE.Mesh(new THREE.PlaneGeometry(1000, 1000, 1, 1),
+                                      new THREE.MeshLambertMaterial({ color: 0x777777 }));
+
+        this.Element.position.set(0, -0.5, 0);
+
+        this.Element.rotation.x = -1 * Math.PI / 2;
+
+        this.Element.receiveShadow = true;
+    }
+});
