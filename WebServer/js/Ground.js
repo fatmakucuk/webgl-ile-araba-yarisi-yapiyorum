7200089ee6e5b8f95deeeefa1c0f34a55dbf97cd
leventkaragol <leventkaragol@gmail.com>
2013-07-25 14:27:21 +0000

    r3 trunk'a Merge edildi

--- WebServer/js/Ground.js
+++ WebServer/js/Ground.js
@@ -2,13 +2,15 @@
     Element: null,
     init: function ()
     {
+        // ileride, üzerinde ağaçların ve diğer güzelliklerin bulunduğu pistimizi içerecek olacan
+        // 1km x 1km boyutlarındaki yüzeyiğimizi tanımlıyoruz ve rengini gri renge boyuyoruz
         this.Element = new THREE.Mesh(new THREE.PlaneGeometry(1000, 1000, 1, 1),
                                       new THREE.MeshLambertMaterial({ color: 0x777777 }));
 
-        this.Element.position.set(0, -0.5, 0);
-
+        // Yüzey sahneye dik olarak eklendiğinden, 90 derece yana yatırıyoruz
         this.Element.rotation.x = -1 * Math.PI / 2;
 
+        // Yüzeyin üzerine gölge düşeceğini belirtiyoruz
         this.Element.receiveShadow = true;
     }
 });
