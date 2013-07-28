cfa38ccbadb6fb778d329ac4ea62dbe07bf9b7de
leventkaragol@gmail.com <leventkaragol@gmail.com@c91ced5c-9e9e-11e6-9eac-1c7dd7574ca4>
2013-07-28 12:45:23 +0000

    r6 için demo dosyaları eklendi

---
+++ wiki/demo/r6/models/CarBody.js
@@ -0,0 +1,74 @@
+{
+
+	"metadata" :
+	{
+		"formatVersion" : 3.1,
+		"generatedBy"   : "Blender 2.66 Exporter",
+		"vertices"      : 16,
+		"faces"         : 12,
+		"normals"       : 8,
+		"colors"        : 0,
+		"uvs"           : [],
+		"materials"     : 1,
+		"morphTargets"  : 0,
+		"bones"         : 0
+	},
+
+	"scale" : 1.000000,
+
+	"materials" : [	{
+		"DbgColor" : 15658734,
+		"DbgIndex" : 0,
+		"DbgName" : "Material.001",
+		"blending" : "NormalBlending",
+		"colorAmbient" : [0.800000011920929, 0.0, 0.0],
+		"colorDiffuse" : [0.800000011920929, 0.0, 0.0],
+		"colorSpecular" : [0.5, 0.5, 0.5],
+		"depthTest" : true,
+		"depthWrite" : true,
+		"shading" : "Lambert",
+		"specularCoef" : 50,
+		"transparency" : 1.0,
+		"transparent" : false,
+		"vertexColors" : false
+	},
+
+	{
+		"DbgColor" : 15597568,
+		"DbgIndex" : 1,
+		"DbgName" : "Material.001",
+		"blending" : "NormalBlending",
+		"colorAmbient" : [0.800000011920929, 0.0, 0.0],
+		"colorDiffuse" : [0.800000011920929, 0.0, 0.0],
+		"colorSpecular" : [0.5, 0.5, 0.5],
+		"depthTest" : true,
+		"depthWrite" : true,
+		"shading" : "Lambert",
+		"specularCoef" : 50,
+		"transparency" : 1.0,
+		"transparent" : false,
+		"vertexColors" : false
+	}],
+
+	"vertices" : [-0.88,0.33,1.74059,-0.88,0.33,-0.759406,0.88,0.33,-0.759406,0.88,0.33,1.74059,-0.88,1.07,1.74059,-0.88,1.07,-0.759406,0.88,1.07,-0.759406,0.88,1.07,1.74059,-0.88,-0.35,2.3,-0.88,-0.35,-2.3,0.88,-0.35,-2.3,0.88,-0.35,2.3,-0.88,0.35,2.3,-0.88,0.35,-2.3,0.88,0.35,-2.3,0.88,0.35,2.3],
+
+	"morphTargets" : [],
+
+	"normals" : [-0.577349,-0.577349,-0.577349,-0.577349,-0.577349,0.577349,-0.577349,0.577349,0.577349,-0.577349,0.577349,-0.577349,0.577349,0.577349,-0.577349,0.577349,-0.577349,-0.577349,0.577349,0.577349,0.577349,0.577349,-0.577349,0.577349],
+
+	"colors" : [],
+
+	"uvs" : [],
+
+	"faces" : [35,1,0,4,5,0,0,1,2,3,35,5,6,2,1,0,3,4,5,0,35,6,7,3,2,0,4,6,7,5,35,0,3,7,4,0,1,7,6,2,35,0,1,2,3,0,1,0,5,7,35,7,6,5,4,0,6,4,3,2,35,9,8,12,13,1,0,1,2,3,35,13,14,10,9,1,3,4,5,0,35,14,15,11,10,1,4,6,7,5,35,8,11,15,12,1,1,7,6,2,35,8,9,10,11,1,1,0,5,7,35,15,14,13,12,1,6,4,3,2],
+
+	"bones" : [],
+
+	"skinIndices" : [],
+
+	"skinWeights" : [],
+
+	"animation" : {}
+
+
+}
