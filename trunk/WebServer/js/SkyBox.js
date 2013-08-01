5b698338d9ad2099638e436c9e1f14959f080996
leventkaragol@gmail.com <leventkaragol@gmail.com@c91ced5c-9e9e-11e6-9eac-1c7dd7574ca4>
2013-08-01 22:47:35 +0000

    r10 trunk'a Merge edildi

--- trunk/WebServer/js/SkyBox.js
+++ trunk/WebServer/js/SkyBox.js
@@ -2,11 +2,8 @@
     Element: null,
     init: function ()
     {
-        // 3D dünyamızın sınırlarını belirleyen 2km x 2km x 2km boyutlarındaki küpümüzü tanımlıyoruz ve rengini gökyüzü rengine boyuyoruz
-        this.Element = new THREE.Mesh(new THREE.CubeGeometry(2000, 2000, 2000),
-                                      new THREE.MeshBasicMaterial({ color: 0xe0eeee, side: THREE.BackSide }));
-
-        // Belirli bir mesafeden sonrasında gökyüzü renginde sislenme yapıyoruz
-        scene.Element.fog = new THREE.FogExp2(0xe0eeee, 0.00025);
+        // 3D dünyamızın sınırlarını belirleyen 4km yarıçağındaki küremizi tanımlıyoruz ve bulut texture ile kaplıyoruz
+        this.Element = new THREE.Mesh(new THREE.SphereGeometry(4000, 3, 3),
+                                      new THREE.MeshBasicMaterial({ map: THREE.ImageUtils.loadTexture("texture/clouds.jpg"), side: THREE.BackSide }));
     }
 });
