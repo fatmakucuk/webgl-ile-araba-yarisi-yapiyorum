58ef205018d71355d2250bf2d6e8bf0e2bb4803e
leventkaragol@gmail.com <leventkaragol@gmail.com@c91ced5c-9e9e-11e6-9eac-1c7dd7574ca4>
2013-08-01 22:49:20 +0000

    r10 branch'ı silindi

--- branches/r10/WebServer/js/SkyBox.js
+++
@@ -1,9 +0,0 @@
-﻿can.Construct("CarGame.SkyBox", {}, {
-    Element: null,
-    init: function ()
-    {
-        // 3D dünyamızın sınırlarını belirleyen 4km yarıçağındaki küremizi tanımlıyoruz ve bulut texture ile kaplıyoruz
-        this.Element = new THREE.Mesh(new THREE.SphereGeometry(4000, 3, 3),
-                                      new THREE.MeshBasicMaterial({ map: THREE.ImageUtils.loadTexture("texture/clouds.jpg"), side: THREE.BackSide }));
-    }
-});
