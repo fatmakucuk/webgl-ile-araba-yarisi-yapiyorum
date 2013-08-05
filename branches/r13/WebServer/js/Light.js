0c2a3db24048e1bcef8f3b148d99f9154d0e8c9e
leventkaragol@gmail.com <leventkaragol@gmail.com@c91ced5c-9e9e-11e6-9eac-1c7dd7574ca4>
2013-08-05 23:29:24 +0000

    r13 için Branch alındı

---
+++ branches/r13/WebServer/js/Light.js
@@ -0,0 +1,72 @@
+﻿/// <reference path="../libs/jquery.min.js" />
+/// <reference path="../libs/can.jquery.min.js" />
+/// <reference path="../libs/three.min.js" />
+
+can.Construct("CarGame.Light", {}, {
+    Element: null,
+    DirectionalLight: null,
+    AmbientLight: null,
+    init: function ()
+    {
+        // Işık grubumuz
+        this.Element = new THREE.Object3D();
+
+        // Gölge düşüren Spot ışığımız
+        this.DirectionalLight = new THREE.DirectionalLight(0xffffff, 2);
+
+        this.DirectionalLight.position.set(500, 500, 500);
+
+        this.DirectionalLight.castShadow = true;
+
+        // Gölge koyuluğu
+        this.DirectionalLight.shadowDarkness = 0.5;
+
+        // Gölge çözünürlüğü
+        this.DirectionalLight.shadowMapWidth = 2048;
+        this.DirectionalLight.shadowMapHeight = 2048;
+
+        // Gölge düşürülecek alan boyutu
+        this.DirectionalLight.shadowCameraNear = 490;
+        this.DirectionalLight.shadowCameraFar = 5000;
+        this.DirectionalLight.shadowCameraLeft = -100;
+        this.DirectionalLight.shadowCameraRight = 100;
+        this.DirectionalLight.shadowCameraTop = 100;
+        this.DirectionalLight.shadowCameraBottom = -100;
+
+        this.Element.add(this.DirectionalLight);
+       
+        
+        // Sahne ışığımız
+        this.AmbientLight = new THREE.AmbientLight(0x454545);
+        
+        this.Element.add(this.AmbientLight);
+    },
+    Animate: function (keyboardState, car)
+    {
+        // Işığın sahnedeki hedefini, arabanın konumuna göre ayarlıyoruz
+        this.SetLightTarget(car);
+
+        this.HandleKeyboard(keyboardState);
+    },
+    HandleKeyboard: function (keyboardState)
+    {
+        if (keyboardState.pressed("n"))
+        {
+            // Gece modu için ışığı kısıyoruz
+            this.DirectionalLight.intensity = 0.2;
+            this.AmbientLight.visible = false;
+        }
+        
+        if (keyboardState.pressed("d"))
+        {
+            // Gündüz modu için ışığı açıyoruz
+            this.DirectionalLight.intensity = 2;
+            this.AmbientLight.visible = true;
+        }
+    },
+    SetLightTarget: function (car)
+    {
+        // Işığın sahnedeki hedefini, arabanın konumuna göre ayarlıyoruz
+        this.DirectionalLight.target.position = car.Element.position;
+    }
+});
