1805a90870e1afae2099f379b5d91371744e4c54
leventkaragol@gmail.com <leventkaragol@gmail.com@c91ced5c-9e9e-11e6-9eac-1c7dd7574ca4>
2013-08-11 13:02:46 +0000

    r15 için tag alındı

---
+++ tags/r15/WebServer/js/Ground.js
@@ -0,0 +1,80 @@
+﻿/// <reference path="../libs/jquery.min.js" />
+/// <reference path="../libs/can.jquery.min.js" />
+/// <reference path="../libs/three.min.js" />
+
+can.Construct("CarGame.Ground", {}, {
+    Element: null,
+    RaceTrackGrass: null,
+    RaceTrackRoad: null,
+    RaceTrackBarrier: null,
+
+    // Yüklemesi beklenen model sayısı
+    RemainingModelCount: 3,
+
+    init: function ()
+    {
+        this.Element = new THREE.Object3D();
+
+        var jsonLoader = new THREE.JSONLoader();
+
+        // Yarış pistinin çim zemin modelini yüklüyoruz
+        jsonLoader.load("models/RaceTrackGrass.js", this.RaceTrackGrassModelLoaded, "texture");
+
+        // Yarış pistinin yol modelini yüklüyoruz
+        jsonLoader.load("models/RaceTrackRoad.js", this.RaceTrackRoadModelLoaded, "texture");
+
+        // Yarış pistinin bariyer modelini yüklüyoruz
+        jsonLoader.load("models/RaceTrackBarrier.js", this.RaceTrackBarrierModelLoaded, "texture");
+    },
+    RaceTrackGrassModelLoaded: function (geometry, materials)
+    {
+        // RaceTrackGrass.js dosyasının yüklenmesi tamamlandığı zaman bu metot işletilir
+        // Yüklenen modeli nesnemize ekliyoruz
+        var material = new THREE.MeshFaceMaterial(materials);
+
+        this.ground.RaceTrackGrass = new THREE.Mesh(geometry, material);
+
+        // Çim ve yol modelleri üst üste gelince titreme olmaması için, çim modelini 3 santim aşağı indiriyoruz
+        this.ground.RaceTrackGrass.position.y -= 0.03;
+
+        // Yüzeyin üzerine gölge düşeceğini belirtiyoruz
+        this.ground.RaceTrackGrass.receiveShadow = true;
+
+        this.ground.Element.add(this.ground.RaceTrackGrass);
+
+        // Yüklemesi beklenen model sayısını bir azaltıyoruz
+        this.ground.RemainingModelCount--
+    },
+    RaceTrackRoadModelLoaded: function (geometry, materials)
+    {
+        // RaceTrackRoad.js dosyasının yüklenmesi tamamlandığı zaman bu metot işletilir
+        // Yüklenen modeli nesnemize ekliyoruz
+        var material = new THREE.MeshFaceMaterial(materials);
+
+        this.ground.RaceTrackRoad = new THREE.Mesh(geometry, material);
+
+        // Yüzeyin üzerine gölge düşeceğini belirtiyoruz
+        this.ground.RaceTrackRoad.receiveShadow = true;
+        
+        this.ground.Element.add(this.ground.RaceTrackRoad);
+
+        // Yüklemesi beklenen model sayısını bir azaltıyoruz
+        this.ground.RemainingModelCount--
+    },
+    RaceTrackBarrierModelLoaded: function (geometry, materials)
+    {
+        // RaceTrackBarrier.js dosyasının yüklenmesi tamamlandığı zaman bu metot işletilir
+        // Yüklenen modeli nesnemize ekliyoruz
+        var material = new THREE.MeshFaceMaterial(materials);
+
+        this.ground.RaceTrackBarrier = new THREE.Mesh(geometry, material);
+
+        // Yüzeyin üzerine gölge düşeceğini belirtiyoruz
+        this.ground.RaceTrackBarrier.receiveShadow = true;
+
+        this.ground.Element.add(this.ground.RaceTrackBarrier);
+
+        // Yüklemesi beklenen model sayısını bir azaltıyoruz
+        this.ground.RemainingModelCount--
+    }
+});
